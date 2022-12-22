b197bc60ccceb86a125b244e3e0c587cee483a6d
rishayshandilya2001 <101794992+rishayshandilya2001@users.noreply.github.com>
2022-12-22 22:06:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function changeoff()
+{
+    var elem = document.getElementById('onn');
+    elem.src='https://i.postimg.cc/KjK1wL3c/bulb-off.png'
+}
+
+function changeon()
+{
+    var elem = document.getElementById('onn');
+    elem.src='https://i.postimg.cc/6QyTynzr/bulb-on.png'
+}
